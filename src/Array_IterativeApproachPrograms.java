a34a74036845c1292efb0d90ff7ce32662220789
shivamstech <shivamsharma.code@gmail.com>
2026-06-04 17:01:43 +0530

    IterativeApproachPrograms

---
+++ src/Array_IterativeApproachPrograms.java
@@ -0,0 +1,42 @@
+
+//It is an approach (programming method) in which a set of instruction is repeatedly
+//executed using looping statements i.e. for, white, do—white until a condition is satisfied.
+
+//Iterative approach used single loop or two independent loops (not nested loops) .
+//Its average time complexity is generally Linear [ O(n) ] or Linearithmic [ log n) ] .
+
+public class Array_IterativeApproachPrograms {
+    public static void main(String[] args) {
+
+        int[] arr={10,20,30,40,50,33,57,97};
+
+//        // WAP to sum att the elements in an array
+//
+//        int sum=0;
+//        for(int i=0; i<arr.length; i++)
+//        {
+//            sum +=arr[i];
+//        }
+//        System.out.println("Total sum of this array is  "+sum);
+
+//        // WAP to count even and odd numbers in an array
+//
+//          int odd=0;
+//          int even=0;
+//
+//          for(int i=0; i<arr.length; i++)
+//          {
+//              if(arr[i]%2==0)
+//              {
+//                  even +=arr[i];
+//
+//              }
+//              else{
+//                    odd +=arr[i];
+//                  }
+//          }
+//
+//          System.out.println("total even inputs  "+even+"  Total odd inputs "+odd);
+
+    }
+}
