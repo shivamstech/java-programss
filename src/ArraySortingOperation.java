947aa6d7452069f6fe4f3010655e4ac2f0939b27
shivamstech <shivamsharma.code@gmail.com>
2026-06-04 13:03:52 +0530

    update commit

--- src/ArraySortingOperation.java
+++ src/ArraySortingOperation.java
@@ -1,11 +1,13 @@
 public class ArraySortingOperation {
 
     public static void main(String[] args) {
+        //  Bubble sort
 
         int[] arr={10,40,30,50,20};
 
         for(int i=0; i<arr.length ; i++)
         {
+            boolean swapped=false;
             for(int j=0; j<arr.length-1; j++)
             {
                 if(arr[j]>arr[j+1])
@@ -14,7 +16,12 @@ public class ArraySortingOperation {
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
+            }
+            if(!swapped)
+            {
+                break;
             }
         }
         for(int a:arr)
