88377d03f6854eda656dab5cb7c6200b67115ba9
shivamstech <shivamsharma.code@gmail.com>
2026-06-07 19:34:12 +0530

    BruteForceApproachTaskupdate commit

--- src/ArrayBruteForceApproachTasks.java
+++ src/ArrayBruteForceApproachTasks.java
@@ -2,25 +2,55 @@ public class ArrayBruteForceApproachTasks {
     public static void main(String[] args) {
 
 
+//        // o WAP to Count the Frequency of Each Element
+//
+//        int[] arr = {10, 20, 10, 30, 20, 40, 10}
+//
+//        int n = arr.length;
+//
+//        for(int i=0; i<n; i++)
+//        {
+//            boolean isRepeated = false;
+//            for(int k=0; k<i; k++)
+//            {
+//                if(arr[i]==arr[k])
+//                {
+//                    isRepeated = true;
+//                    break;
+//                }
+//            }
+//
+//            if(isRepeated)
+//            {
+//                continue;
+//            }
+//
+//            int count = 1;
+//            for(int j=i+1; j<n; j++)
+//            {
+//                if(arr[i] == arr[j])
+//                {
+//                    count++;
+//                }
+//            }
+//
+//            System.out.println(arr[i]+" -> "+count+" times");
+//        }
+
+        //==================================================================
+
         // o WAP to Count the Frequency of Each Element
 
-        int[] arr = {10, 20, 10, 30, 20, 40, 10}
+
+        int[] arr = {10, 20, 10, 30, 20, 40, 10};
 
         int n = arr.length;
 
+        boolean[] bArr = new boolean[n];
+
         for(int i=0; i<n; i++)
         {
-            boolean isRepeated = false;
-            for(int k=0; k<i; k++)
-            {
-                if(arr[i]==arr[k])
-                {
-                    isRepeated = true;
-                    break;
-                }
-            }
-
-            if(isRepeated)
+            if(bArr[i])
             {
                 continue;
             }
@@ -31,6 +61,7 @@ public class ArrayBruteForceApproachTasks {
                 if(arr[i] == arr[j])
                 {
                     count++;
+                    bArr[j] = true;
                 }
             }
 
