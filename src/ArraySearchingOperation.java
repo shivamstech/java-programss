86748ff69bd2b74debdb9107196f8a2ebf6f4254
shivamstech <shivamsharma.code@gmail.com>
2026-06-11 17:45:26 +0530

    ArraySearchOperationUpdate1 commit

--- src/ArraySearchingOperation.java
+++ src/ArraySearchingOperation.java
@@ -2,31 +2,72 @@ public class ArraySearchingOperation {
 
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 40, 50};
 
-        int element = 40;
+//        //Linear Search
+//
+//        int[] arr = {10, 20, 30, 40, 50};
+//
+//        int element = 40;
+//
+//        int pos = -1;
+//
+//        int n = arr.length;
+//
+//        for(int i=0; i<n; i++)
+//        {
+//            if(arr[i] == element)
+//            {
+//                pos = i;
+//                break;
+//            }
+//        }
+//
+//        if(pos != -1)
+//        {
+//            System.out.println("Element found at "+pos+" index position");
+//        }
+//        else
+//        {
+//            System.out.println("Element not found");
+//        }
 
-        int pos = -1;
+          //===================================================================
 
-        int n = arr.length;
-
-        for(int i=0; i<n; i++)
-        {
-            if(arr[i] == element)
-            {
-                pos = i;
-                break;
-            }
-        }
-
-        if(pos != -1)
-        {
-            System.out.println("Element found at "+pos+" index position");
-        }
-        else
-        {
-            System.out.println("Element not found");
-        }
-
+           //  Binary Search.
+        //
+        //
+        //        int[] arr = {6, 8, 11, 21, 27, 32, 42, 55, 73, 81, 87, 93};
+        //
+        //        int element = 57;
+        //
+        //        int li = 0;
+        //        int hi = arr.length - 1;
+        //
+        //        boolean isFound = false;
+        //
+        //        while(li <= hi)
+        //        {
+        //            int mid = (li+hi) / 2;
+        //
+        //            if(arr[mid] == element)
+        //            {
+        //                System.out.println("Element found at index position : "+mid);
+        //                isFound = true;
+        //                break;
+        //            }
+        //            if(element > arr[mid])
+        //            {
+        //                li = mid + 1;
+        //            }
+        //            else
+        //            {
+        //                hi = mid - 1;
+        //            }
+        //        }
+        //
+        //        if(!isFound)
+        //        {
+        //            System.out.println("Element not found");
+        //        }
     }
 }
