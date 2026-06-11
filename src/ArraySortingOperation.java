c2b8808646f14cc4697beeb56de4c2bcec3d78a1
shivamstech <shivamsharma.code@gmail.com>
2026-06-11 17:51:18 +0530

    ArraySortingOperationUpdate2 commit

--- src/ArraySortingOperation.java
+++ src/ArraySortingOperation.java
@@ -32,27 +32,54 @@ public class ArraySortingOperation {
 
         //=================================================================
 
-        // * Selection Sort
+//        // * Selection Sort
+//
+//        int[] arr = {32, 12, 25, 18, 42, 83, 91, 34, 55, 71, 6, 28};
+//
+//        int n = arr.length;
+//
+//        for(int i=0; i<n; i++)
+//        {
+//            int minIndex = i;
+//
+//            for(int j=i+1; j<n; j++)
+//            {
+//                if(arr[minIndex] > arr[j])
+//                {
+//                    minIndex = j;
+//                }
+//            }
+//
+//            int temp = arr[minIndex];
+//            arr[minIndex] = arr[i];
+//            arr[i] = temp;
+//        }
+//
+//        for(int no : arr)
+//        {
+//            System.out.print(no+" ");
+//        }
+
+        //==================================================================
+
+        // * Insertion Sort
 
         int[] arr = {32, 12, 25, 18, 42, 83, 91, 34, 55, 71, 6, 28};
 
         int n = arr.length;
 
-        for(int i=0; i<n; i++)
+        for(int i=1; i<n; i++)
         {
-            int minIndex = i;
+            int j = i-1;
+            int temp = arr[i];
 
-            for(int j=i+1; j<n; j++)
+            while(j>=0 && arr[j]>temp)
             {
-                if(arr[minIndex] > arr[j])
-                {
-                    minIndex = j;
-                }
+                arr[j+1] = arr[j];
+                j--;
             }
 
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+            arr[j+1] = temp;
         }
 
         for(int no : arr)
