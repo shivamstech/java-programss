2190c33cf083aab9a0bc127f66b67732a9ceafbe
shivamstech <shivamsharma.code@gmail.com>
2026-06-12 21:27:06 +0530

    TwoPointersPrograms1 commit1

---
+++ src/Array_TwoPointersPrograms.java
@@ -0,0 +1,31 @@
+public class Array_TwoPointersPrograms {
+
+//    static boolean isPalindrome(int[] arr)
+//    {
+//        int L = 0;
+//        int R = arr.length - 1;
+//
+//        while(L < R)
+//        {
+//            if(arr[L] != arr[R])
+//            {
+//                return false;
+//            }
+//
+//            L++;
+//            R--;
+//        }
+//
+//        return true;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[] arr = {1,2,4,4,3,2,1};
+//
+//        System.out.println(isPalindrome(arr));
+//    }
+
+
+
+}
