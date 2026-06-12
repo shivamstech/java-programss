89d072caf25ff32b794575b322261b43894859fc
shivamstech <shivamsharma.code@gmail.com>
2026-06-12 21:29:01 +0530

    HelloWorld commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,69 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-//        System.out.println("Hello World");
-//
-//        System.out.println("First program");
-//
-//        System.out.println("HelloWorld program");
+        System.out.println("Hello World");
 
-//       int[] arr= {32, 12, 25, 18, 42, 83, 91, 34, 55, 71, 6, 28};
-//
-//       for(int i=0; i<arr.length-1; i++)
-//       {
-//           for(int j=i+1; j>=0 ; j--)
-//           {
-//               if(arr[i]>arr[j])
-//               {
-//                 int temp=arr[i];
-//                 arr[i]=arr[j];
-//                 arr[j]=temp;
-//               }
-//           }
-//       }
-//       for(int a:arr)
-//       {
-//           System.out.print(" "+a);
-//       }
+        System.out.println("First program");
 
-//        int[] arr = {32, 12, 25, 18, 42, 83, 91, 34, 55, 71, 6, 28};
-//
-//        for(int i=1; i<arr.length; i++ )
-//        {
-//            int temp=arr[i];
-//            int j=i-1;
-//
-//            while(j>=0 && temp<arr[j] ){
-//
-//                arr[j+1]=arr[j];
-//
-//                j--;
-//            }
-//            arr[j+1]=temp;
-//        }
-//        for(int a:arr)
-//        {
-//            System.out.print(" "+a);
-//        }
-
-
-        //=====================================================================================
-
-//        int[] arr={10,30,20,40,60,50};
-//
-//        int element=40;
-//
-//        int li=0;
-
-//
-//        for(int i=0; i<arr.length; i++)
-//        {
-
-//        }
-//
-
-        //=============================================================
-
+        System.out.println("HelloWorld program");
 
 
     }
